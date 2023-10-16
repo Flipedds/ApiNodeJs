0cd0c0dbbdba234e97fccc76ac4178edf0b3658c
Filipe André <flipeandre12@gmail.com>
2023-10-16 12:40:38 -0300

    feat: rota de post pet

--- Routes/pet.route.js
+++ Routes/pet.route.js
@@ -1,18 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Pet = require("../Models/Pet");
 
-/**
- * @swagger
- * /pet/:
- *   get:
- *     summary: Retorna home
- *     responses:
- *       200:
- *         description: Sucesso
- */
-router.get("/", (req, res) =>
-{
-    res.send("Bem vindo aos pets");
-})
+router.get("/", async (req, res) => {
+  res.send("Bem vindo aos pets");
+});
+
+router.post("/criar", async (req, res) => {
+  const { nome, especie, cor, idade, foto, peso } = req.body;
+
+  if (!nome && String(nome).length > 0) {
+    return res.status(422).json({ msg: "O nome é obrigatório!" });
+  } else if (!especie && String(especie).length > 0) {
+    return res.status(422).json({ msg: "A espécie é obrigatória!" });
+  } else if (!cor && String(cor).length > 0) {
+    return res.status(422).json({ msg: "A cor é obrigatória!" });
+  } else if (!idade && Number(idade) > 0) {
+    return res.status(422).json({ msg: "A idade é obrigatória" });
+  } else if (!foto && String(foto).length > 0) {
+    return res.status(422).json({ msg: "Foto é obrigatória" });
+  } else if (!peso && Number(peso) > 0) {
+    return res.status(422).json({ msg: "Peso é obrigatório" });
+  }
+
+  const pet = new Pet({ nome, especie, cor, idade, foto, peso });
+
+  try {
+    await pet.save();
+    res.status(201).json({ msg: "Pet criado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      msg: "Aconteceu um erro no servidor, tente novamente mais tarde !",
+    });
+  }
+});
 
 module.exports = router;
--- swaggerDef.js
+++ swaggerDef.js
@@ -1,16 +1,84 @@
 const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-      title: 'Minha API Express com Swagger',
-      version: '1.0.0',
-      description: 'Documentação da API Express com Swagger',
+  openapi: '3.0.0',
+  info: {
+    title: 'Minha API Express com Swagger',
+    version: '1.0.0',
+    description: 'Documentação da API Express com Swagger',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Servidor local',
     },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Servidor local',
+  ],
+  paths: {
+    '/pet/criar': {
+      post: {
+        summary: 'Cria um novo pet',
+        tags: ['Pets'],
+        requestBody: {
+          description: 'Dados do pet a ser criado',
+          required: true,
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                properties: {
+                  nome: {
+                    type: 'string',
+                    example: 'Nome do animal',
+                  },
+                  especie: {
+                    type: 'string',
+                    example: 'Espécie do animal',
+                  },
+                  cor: {
+                    type: 'string',
+                    example: 'Cor do animal',
+                  },
+                  idade: {
+                    type: 'number',
+                    example: 3,
+                  },
+                  foto: {
+                    type: 'string',
+                    example: 'URL da foto do animal',
+                  },
+                  peso: {
+                    type: 'number',
+                    example: 5.5,
+                  },
+                },
+                required: ['nome', 'especie'],
+              },
+            },
+          },
+        },
+        responses: {
+          '201': {
+            description: 'Pet criado com sucesso',
+          },
+          '400': {
+            description: 'Requisição inválida. Verifique os dados de entrada.',
+          },
+        },
       },
-    ],
-  };
-  
+    },
+    '/pet/': {
+      get: {
+        summary: 'Retorna todos os pets',
+        tags: ['Pets'],
+        responses: {
+          '201': {
+            description: 'Pet criado com sucesso',
+          },
+          '400': {
+            description: 'Requisição inválida',
+          },
+        },
+      }
+    }
+  },
+};
+
 module.exports = swaggerDefinition;
